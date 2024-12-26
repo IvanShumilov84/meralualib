4e06346ca41c4fc4e50139f3a6b30875841562d1
Ivan Shumilov <shumilov@nppmera.ru>
2024-12-26 18:14:46 +0300

    Update convertfunc module

--- meralualib/convertfunc.lua
+++ meralualib/convertfunc.lua
@@ -1,40 +1,58 @@
 --[[
--- Библиотека фирмы МЕРА.
--- Версия: v0
+-- Модуль конвертации величин.
 --]]
 
-convertfunc = {}
+local t = {}
 
 
-do  -- Функция конвертации real в bool.
-  --[[
-      Пример вызова:
-      local real_to_bool = lib["real_to_bool"]
-      local bool_value = real_to_bool(real_value)
-  ]]
-  function real_to_bool(real_value)
-      assert(real_value ~= nil , "real_value is nil ")
-      assert(type(real_value) ~= "string", "real_value is string ")
-
-      return real_value > 0
-  end
-  convertfunc["real_to_bool"] = real_to_bool
+do --  Функция конвертации real в bool.
+    ---@param value number
+    ---@return boolean
+    function t.real_to_bool(value)
+        assert(type(value) == "number", "Parameter 'value': expected 'number', got '" .. type(value) .. "'. ")
+        return value > 0
+    end
 end
 
 
-do  -- Функция конвертации bool в real.
-  --[[
-      Пример вызова:
-      local bool_to_real = lib["bool_to_real"]
-      local real_value = bool_to_real(bool_value)
-  ]]
-  function bool_to_real(bool_value)
-      assert(bool_value ~= nil , "bool_value is nil ")
-      assert(type(bool_value) ~= "string", "bool_value is string ")
-      return bool_value and 1 or 0
-  end
-  convertfunc["bool_to_real"] = bool_to_real
+do --  Функция конвертации bool в real.
+    ---@param value boolean
+    ---@return number
+    function t.bool_to_real(value)
+        assert(type(value) == "boolean", "Parameter 'value': expected 'boolean', got '" .. type(value) .. "'. ")
+        return value and 1 or 0
+    end
 end
 
 
-return convertfunc
+do --  Преобразование градусов Цельсия в градусы Фаренгейта.
+    ---@param value number
+    ---@return number
+    function t.Celsius_to_Fahrenheit(value)
+        assert(type(value) == "number", "Parameter 'value': expected 'number', got '" .. type(value) .. "'. ")
+        return 9 / 5 * value + 32
+    end
+end
+
+
+do --  Преобразование градусов Цельсия в Кельвины.
+    ---@param value number
+    ---@return number
+    function t.Celsius_to_Kelvin(value)
+        assert(type(value) == "number", "Parameter 'value': expected 'number', got '" .. type(value) .. "'. ")
+        return value + 273.15
+    end
+end
+
+
+do --  Преобразование Кельвинов в градусы Цельсия.
+    ---@param value number
+    ---@return number
+    function t.Kelvin_to_Celsius(value)
+        assert(type(value) == "number", "Parameter 'value': expected 'number', got '" .. type(value) .. "'. ")
+        return value - 273.15
+    end
+end
+
+
+return t
