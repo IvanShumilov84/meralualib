b1ef5b6b6a7f5811ec47af0ab3c183e359dc930f
Ivan Shumilov <shumilov@nppmera.ru>
2025-02-26 15:31:00 +0300

    Add datatype, change collect modules

--- meralualib/collect.lua
+++ meralualib/collect.lua
@@ -6,10 +6,10 @@ local t = {}
 
 
 do --  Функция поиска ключа по значению в словаре.
-    ---@param dict table
-    ---@param value number
-    ---@param default string
-    ---@return string
+    ---@param dict table Словарь.
+    ---@param value number Искомое значение.
+    ---@param default string Возвращаемый ключ в случае отсутствия искомого значения.
+    ---@return string key Ключ искомого значения.
     function t.dict_find_key(dict, value, default)
         assert(type(dict) == "table", "Parameter 'dict': expected 'table', got '" .. type(dict) .. "'. ")
         assert(type(value) == "number", "Parameter 'value': expected 'number', got '" .. type(value) .. "'. ")
@@ -34,6 +34,7 @@ do --  Функция расширения списка.
     function t.list_extend(list_1, list_2)
         assert(type(list_1) == "table", "Parameter 'list_1': expected 'table', got '" .. type(list_1) .. "'. ")
         assert(type(list_2) == "table", "Parameter 'list_2': expected 'table', got '" .. type(list_2) .. "'. ")
+        if not #list_2 then return list_1 end
         for _, v in ipairs(list_2) do table.insert(list_1, v) end
         return list_1
     end
